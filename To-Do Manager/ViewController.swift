603ad0f8295293bcc2567c618a74d5973d8bf7a2
boomag77 <96821826+boomag77@users.noreply.github.com>
2022-08-07 19:27:40 +0300

    Initial Commit

---
+++ To-Do Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do Manager
+//
+//  Created by Sergey on 8/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
